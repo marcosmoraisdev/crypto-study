82b594491443e1f0527c32e588c7f9eee9e511c1
Marcos Morais <marcosmoraisdev@gmail.com>
2023-06-28 16:06:50 +0100

    crypto zombies lesson 5

--- CryptoZombies/zombiefeeding.sol
+++ CryptoZombies/zombiefeeding.sol
@@ -25,7 +25,7 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory {
     KittyInterface kittyContract;
 
-    modifier ownerOf(uint _zombieId) {
+    modifier onlyOwnerOf(uint _zombieId) {
         require(msg.sender == zombieToOwner[_zombieId]);
         _;
     }
@@ -46,7 +46,7 @@ contract ZombieFeeding is ZombieFactory {
         uint _zombieId,
         uint _targetDna,
         string _species
-    ) internal ownerOf(_zombieId) {
+    ) internal onlyOwnerOf(_zombieId) {
         Zombie storage myZombie = zombies[_zombieId];
         require(_isReady(myZombie));
         _targetDna = _targetDna % dnaModulus;
