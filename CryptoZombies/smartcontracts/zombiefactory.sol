f95b742d8d659d24c456a9697b812dc20f191f10
Marcos Morais <marcosmoraisdev@gmail.com>
2023-06-29 16:20:29 +0100

    chainlink basic usage

--- CryptoZombies/smartcontracts/zombiefactory.sol
+++ CryptoZombies/smartcontracts/zombiefactory.sol
@@ -1,7 +1,8 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity ^0.6.6;
 
 import "./ownable.sol";
 import "./safemath.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract ZombieFactory is Ownable {
     using SafeMath for uint256;
@@ -13,6 +14,10 @@ contract ZombieFactory is Ownable {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
     uint cooldownTime = 1 days;
+
+    bytes32 public keyHash;
+    uint256 public fee;
+    uint256 public randomResult;
 
     struct Zombie {
         string name;
@@ -28,7 +33,29 @@ contract ZombieFactory is Ownable {
     mapping(uint => address) public zombieToOwner;
     mapping(address => uint) ownerZombieCount;
 
-    function _createZombie(string _name, uint _dna) internal {
+    constructor()
+        public
+        VRFConsumerBase(
+            0x6168499c0cFfCaCD319c818142124B7A15E857ab, // VRF Coordinator
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709 // LINK Token
+        )
+    {
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 100000000000000000;
+    }
+
+    function getRandomNumber() public returns (bytes32 requestId) {
+        return requestRandomness(keyHash, fee);
+    }
+
+    function fulfillRandomness(
+        bytes32 requestId,
+        uint256 randomness
+    ) internal override {
+        randomResult = randomness;
+    }
+
+    function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(
             Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)
         ) - 1;
@@ -37,14 +64,9 @@ contract ZombieFactory is Ownable {
         emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns (uint) {
-        uint rand = uint(keccak256(abi.encodePacked(_str)));
-        return rand % dnaModulus;
-    }
-
     function createRandomZombie(string _name) public {
         require(ownerZombieCount[msg.sender] == 0);
-        uint randDna = _generateRandomDna(_name);
+        uint randDna = randomResult;
         randDna = randDna - (randDna % 100);
         _createZombie(_name, randDna);
     }
